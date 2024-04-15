ee9d7314d3bcecd39fb969b362443f04166052c0
Vanessa Kasminoff <sunnset07@gmail.com>
2024-04-14 19:21:55 -0700

    Refactored code to remove other components except the loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,8 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import { SearchBar } from "./components/SearchBar";
-import { DisplayCard } from "./components/DisplayCard";
 import { Loading } from "./components/Loading";
 
 function App() {
-  //SEARCHBAR
-  const [searchResult, setSearchResult] = useState("");
-
-  const handleSearch = (query) => {
-    if (!query) {
-      setSearchResult("");
-      return;
-    }
-
-    // Normalize the input for consistency
-    const searchQuery = query.trim().toLowerCase();
-
-    // Fetch the Pokemon data
-    fetch(`https://pokeapi.co/api/v2/pokemon/${searchQuery}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Pokemon not found");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // Assuming you want to display the Pokemon's name
-        setSearchResult(data.name);
-      })
-      .catch((error) => {
-        // Handle the case where the Pokemon is not found
-        setSearchResult("Pokemon not found");
-      });
-  };
 
   //LOADING SCREEN
   const [loading, setLoading] = useState(false);
@@ -56,16 +25,7 @@ function App() {
         
         :
 
-        <div className="pokedex">
-          <div className="red-container left">
-          <div className="search-bar-container">
-            <SearchBar onEnter={handleSearch} />
-            {searchResult && <h1>{searchResult}</h1>}
-          </div>
-          <DisplayCard/>
-          </div>
-          <div className="red-container right">Test</div>
-        </div>
+        <div><h1>TEAM ROCKET GO!</h1></div>
       }
     </div>
   );
